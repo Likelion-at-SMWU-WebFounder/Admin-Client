16793a9ffbaf3423a5eebe9c021d49e7e4dfbab9
eunxoo <dmstn7989@naver.com>
2023-11-28 03:17:30 +0900

    [FEAT] 지원현황 페이지 및 Board 컴포넌트

---
+++ src/components/Posts.jsx
@@ -0,0 +1,183 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const StyledInput = styled.input`
+  position: relative;
+  /* margin-left: 10px; */
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  appearance: none;
+  top: 4px;
+  width: 20px;
+  height: 20px;
+  border: 1.5px solid gainsboro;
+  border-radius: 5px;
+  background-color: white;
+  transition: background-color 0.3s, border-color 0.3s;
+
+  &:checked {
+    border-color: transparent;
+    background-size: 100% 100%;
+    background-position: 50%;
+    background-repeat: no-repeat;
+    background-color: #eb9537;
+
+    &::before {
+      content: "✔"; /* 표시할 내용 (체크 표시) */
+      font-size: 14px; /* 표시할 내용의 크기 조절 */
+      color: black; /* 표시할 내용의 색상 */
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
+  }
+`;
+
+const Table = styled.div`
+  display: table;
+  /* flex-direction: column; */
+  width: 100%;
+  border-collapse: collapse;
+`;
+
+const ContentWrap = styled.div`
+  display: table-row-group;
+`;
+
+const ContentTitle = styled.div`
+  background-color: rgba(217, 217, 217, 0.3);
+  border-top: 1px solid white;
+  border-bottom: 1px solid white;
+  font-size: 25px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  letter-spacing: -1.263px;
+  display: table-row;
+  /* flex-direction: row; */
+  /* justify-content: space-between; */
+  /* flex: 1; */
+`;
+
+const TableCell = styled.div`
+  display: table-cell;
+  padding: 8px;
+`;
+
+const Cell = styled.div`
+  display: table-cell;
+  padding: 8px;
+  /* border: 1px solid white; */
+  /* flex-direction: column; */
+`;
+
+const Button = styled.button`
+  display: table-cell;
+  border-radius: 15px;
+  border: none;
+  background: #d9d9d9;
+  width: 139px;
+  height: 31px;
+  /* align-items: center; */
+  flex-shrink: 0;
+  /* margin: 2px; */
+  color: #111;
+  text-align: center;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  letter-spacing: -1.01px;
+`;
+
+const Content = styled.div`
+  /* display: flex;
+  flex-direction: row; */
+  /* justify-content: space-between; */
+  display: table-row;
+  align-items: center;
+  border-bottom: 1px solid white;
+  font-size: 25px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: -1.263px;
+  /* margin-bottom: 10px; */
+`;
+
+const ImageWrap = styled.div``;
+
+const Posts = ({ list }) => {
+  const [checkedItems, setCheckedItems] = useState([]);
+
+  // 전체 체크
+  const onCheckBoxAll = (e) => {
+    if (e.target.checked) {
+      const checkedListArr = [];
+      list.forEach((item) => checkedListArr.push(item.id));
+      setCheckedItems(checkedListArr);
+    } else {
+      setCheckedItems([]);
+    }
+  };
+
+  // 개별 체크
+  // 체크된 아이템의 아이디를 배열 형태로 관리
+  // 체크가 false이면 해당 item의 id가 아닌 것들만 담음.
+  const onChangeCheckBox = (e, item) => {
+    const isChecked = e.target.checked;
+    setCheckedItems((prevCheckedItems) => {
+      if (isChecked) {
+        return [...prevCheckedItems, item.id];
+      } else {
+        return prevCheckedItems.filter((no) => no !== item.id);
+      }
+    });
+  };
+
+  return (
+    <Table>
+      <ContentWrap>
+        <ContentTitle>
+          <TableCell>
+            <StyledInput type="checkbox" onChange={(e) => onCheckBoxAll(e)} />
+          </TableCell>
+          <TableCell>번호</TableCell>
+          <TableCell>이름</TableCell>
+          <TableCell>전화번호</TableCell>
+          <TableCell>학번</TableCell>
+          <TableCell>지원트랙</TableCell>
+          <TableCell>서류 제출 시간</TableCell>
+          <TableCell></TableCell>
+        </ContentTitle>
+        {list !== undefined ? (
+          list.map((data, idx) => (
+            <Content key={idx}>
+              <Cell>
+                <StyledInput
+                  type="checkbox"
+                  onChange={(e) => onChangeCheckBox(e, data)}
+                  checked={checkedItems.includes(data.id)}
+                />
+              </Cell>
+              <Cell>{data.id}</Cell>
+              <Cell>{data.name}</Cell>
+              <Cell>{data.phone}</Cell>
+              <Cell>{data.num}</Cell>
+              <Cell>{data.track}</Cell>
+              <Cell>{data.time}</Cell>
+              <Button>서류 확인</Button>
+            </Content>
+          ))
+        ) : (
+          <ImageWrap>
+            <img src={`${process.env.PUBLIC_URL}/Logo.svg`} alt="loadingImg" />
+          </ImageWrap>
+        )}
+      </ContentWrap>
+    </Table>
+  );
+};
+
+export default Posts;
--- src/pages/recruit/ApplicationStatusPage.jsx
+++ src/pages/recruit/ApplicationStatusPage.jsx
@@ -1,7 +1,153 @@
 import React from "react";
+import styled from "styled-components";
+import * as S from "../../style/LayoutStyle";
+import Navbar from "../../components/Navbar";
+import Logo from "../../components/Logo";
+import Board from "../../components/Board";
+
+const StateContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 70px;
+`;
+
+const StateBox = styled.div`
+  position: relative;
+  width: 239px;
+  height: 153px;
+  flex-shrink: 0;
+  border-radius: 15px;
+  background: rgba(217, 217, 217, 0.6);
+  margin-right: 1.5vw;
+`;
+
+const StateItem = styled.div`
+  position: absolute;
+  left: 15px;
+  top: 17px;
+  color: #111;
+  font-size: 25px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  letter-spacing: -1.263px;
+`;
+
+const StateNum = styled.div`
+  position: absolute;
+  right: 15px;
+  bottom: 17px;
+  color: #111;
+  text-align: right;
+  font-size: 45px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  letter-spacing: -2.273px;
+`;
+
+const ButtonSet = styled.div`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 80px;
+`;
+
+const PDFButton = styled.button`
+  border: none;
+  margin-right: 20px;
+  border-radius: 5px;
+  background: #fafafa;
+  width: 163px;
+  height: 56px;
+  flex-shrink: 0;
+  color: #000;
+  text-align: center;
+  font-size: 28px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  letter-spacing: -1.414px;
+`;
+
+const ResetButton = styled.button`
+  border: none;
+  margin-right: 20px;
+  border-radius: 5px;
+  background: #f4a6a6;
+  width: 197px;
+  height: 56px;
+  flex-shrink: 0;
+  color: #000;
+  text-align: center;
+  font-size: 28px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  letter-spacing: -1.414px;
+`;
+
+const AddButton = styled.button`
+  border: none;
+  margin-right: 20px;
+  border-radius: 5px;
+  background: #8fe088;
+  width: 315px;
+  height: 56px;
+  flex-shrink: 0;
+  color: #000;
+  text-align: center;
+  font-size: 28px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  letter-spacing: -1.414px;
+`;
 
 const ApplicationStatusPage = () => {
-  return <div></div>;
+  return (
+    <>
+      <Logo />
+      <S.Layout>
+        <Navbar />
+        <S.Container>
+          <S.Title>지원 현황 및 지원 서류</S.Title>
+          <S.About>
+            신규모집 지원현황을 확인하고, 지원 서류를 관리합니다.
+          </S.About>
+          <S.SubTitle>지원 현황</S.SubTitle>
+          <StateContainer>
+            <StateBox>
+              <StateItem>전체 지원자 수</StateItem>
+              <StateNum>133명</StateNum>
+            </StateBox>
+            <StateBox>
+              <StateItem>전체 지원자 수</StateItem>
+              <StateNum>133명</StateNum>
+            </StateBox>
+            <StateBox>
+              <StateItem>전체 지원자 수</StateItem>
+              <StateNum>133명</StateNum>
+            </StateBox>
+            <StateBox>
+              <StateItem>전체 지원자 수</StateItem>
+              <StateNum>133명</StateNum>
+            </StateBox>
+          </StateContainer>
+          <Board />
+          <S.ButtonContainer>
+            <ButtonSet>
+              <PDFButton>PDF 추출</PDFButton>
+              <ResetButton>지원자 초기화</ResetButton>
+              <AddButton>합격자 테이블에 추가 + </AddButton>
+            </ButtonSet>
+          </S.ButtonContainer>
+        </S.Container>
+      </S.Layout>
+    </>
+  );
 };
 
 export default ApplicationStatusPage;
