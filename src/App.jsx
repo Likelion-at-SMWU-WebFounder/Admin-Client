bdf2095d265682962014d027bf93bb14b1a72691
eunxoo <dmstn7989@naver.com>
2024-02-10 02:18:54 +0900

    [FEAT] api 모듈화 및 라우터 추가

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/home/HomePage";
+import LoginPage from "./pages/login/LoginPage";
+import ApplicationStatusPage from "./pages/recruit/ApplicationStatusPage";
+import PassedApplicantsPage from "./pages/recruit/PassedApplicantsPage";
+import DocumentItemsPage from "./pages/recruit/DocumentItemsPage";
+import { ThemeProvider } from "styled-components";
+import theme from "./style/theme";
+import GlobalStyle from "./style/GlobalStyle";
+import DocumentItemsEditPage from "./pages/recruit/DocumentItemsEditPage";
+import DocumentDetailPage from "./pages/recruit/DocumentDetailPage";
+import InterviewTimePage from "./pages/recruit/InterviewTimePage";
+import PassFinalPage from "./pages/recruit/PassFinalPage";
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Router>
+        <Routes>
+          <Route path="/sooklion-admin/login" element={<LoginPage />} />
+          {/* <Route element={<Navbar />}> */}
+          <Route path="/sooklion-admin" element={<HomePage />} />
+          <Route
+            path="/sooklion-admin/apply"
+            element={<ApplicationStatusPage />}
+          />
+          <Route
+            path="/sooklion-admin/pass"
+            element={<PassedApplicantsPage />}
+          />
+          <Route
+            path="/sooklion-admin/document"
+            element={<DocumentItemsPage />}
+          />
+          <Route
+            path="/sooklion-admin/editdocument"
+            element={<DocumentItemsEditPage />}
+          />
+          <Route
+            path="/sooklion-admin/apply/:joinerId"
+            element={<DocumentDetailPage />}
+          />
+          <Route
+            path="/sooklion-admin/interview"
+            element={<InterviewTimePage />}
+          />
+          <Route
+            path="/sooklion-admin/pass-final"
+            element={<PassFinalPage />}
+          />
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+export default App;
