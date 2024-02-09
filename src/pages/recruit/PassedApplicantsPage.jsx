9b0286323878e7c38477b16a5c8d2f2117e1ba4f
eunxoo <dmstn7989@naver.com>
2024-02-10 02:16:15 +0900

    [FEAT] 합격자 선정 삭제 api

--- src/pages/recruit/PassedApplicantsPage.jsx
+++ src/pages/recruit/PassedApplicantsPage.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import apiModule from "../../api/apiModule";
 import styled from "styled-components";
 import * as S from "../../style/LayoutStyle";
 import Navbar from "../../components/Navbar";
@@ -23,6 +24,30 @@ const DeleteButton = styled.button`
 `;
 
 const PassedApplicantsPage = () => {
+  const [docs, setDocs] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const data = await apiModule.fetchDocsResult();
+      setDocs(data);
+    } catch (error) {
+      console.error("error:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const deleteDocsResult = async (checkedItems) => {
+    try {
+      await apiModule.deleteDocsResult(checkedItems);
+      await fetchData();
+    } catch (error) {
+      console.error("error:", error);
+    }
+  };
+
   return (
     <>
       <Logo />
@@ -31,13 +56,8 @@ const PassedApplicantsPage = () => {
         <S.Container>
           <S.Title>합격자 선정</S.Title>
           <S.About>합격서류를 분류하여 별도로 관리합니다.</S.About>
-          <S.SubTitle>합격자 테이블</S.SubTitle>
-          <Board />
-          <S.ButtonContainer>
-            <S.ButtonSet>
-              <DeleteButton>삭제</DeleteButton>
-            </S.ButtonSet>
-          </S.ButtonContainer>
+          <S.SubTitle>서류 합격자 테이블</S.SubTitle>
+          <Board pass={docs} type="type2" onDelete={deleteDocsResult} />
         </S.Container>
       </S.Layout>
     </>
